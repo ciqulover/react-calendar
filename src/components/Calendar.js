402cc4b1a1cb07b03b25755e8b72ccc3b3403981
Lei Jiang <ycwalker@outlook.com>
2016-08-24 23:10:02 +0800

    添加注释
    添加注释

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,12 +7,21 @@ import CalendarFooter from './CalendarFooter'
 
 const displayDaysPerMonth = (year)=> {
 
+  //定义每个月的天数，如果是闰年第二月改为29天
   let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     daysInMonth[1] = 29
   }
+
+  //以下为了获取一年中每一个月在日历选择器上显示的数据，
+  //从上个月开始，接着是当月，最后是下个月开头的几天
+
+  //定义一个数组，保存上一个月的天数
+
   let daysInPreviousMonth = [].concat(daysInMonth)
   daysInPreviousMonth.unshift(daysInPreviousMonth.pop())
+
+  //获取每一个月显示数据中需要补足上个月的天数
   let addDaysFromPreMonth = new Array(12)
     .fill(null)
     .map((item, index)=> {
@@ -24,6 +33,7 @@ const displayDaysPerMonth = (year)=> {
       }
     })
 
+  //已数组形式返回一年中每个月的显示数据,每个数据为6行*7天
   return new Array(12)
     .fill([])
     .map((month, monthIndex)=> {
@@ -31,12 +41,15 @@ const displayDaysPerMonth = (year)=> {
         daysCount = daysInMonth[monthIndex],
         daysCountPrevious = daysInPreviousMonth[monthIndex],
         monthData = []
+      //补足上一个月
       for (; addDays > 0; addDays--) {
         monthData.unshift(daysCountPrevious--)
       }
+      //添入当前月
       for (let i = 0; i < daysCount;) {
         monthData.push(++i)
       }
+      //补足下一个月
       for (let i = 42 - monthData.length, j = 0; j < i;) {
         monthData.push(++j)
       }
@@ -46,6 +59,7 @@ const displayDaysPerMonth = (year)=> {
 
 class Calendar extends React.Component {
   constructor() {
+    //继承React.Component
     super()
     let now = new Date()
     this.state = {
@@ -56,6 +70,7 @@ class Calendar extends React.Component {
     }
   }
 
+  //切换到下一个月
   nextMonth() {
     if (this.state.month === 11) {
       this.setState({
@@ -68,7 +83,7 @@ class Calendar extends React.Component {
       })
     }
   }
-
+  //切换到上一个月
   prevMonth() {
     if (this.state.month === 0) {
       this.setState({
@@ -81,17 +96,17 @@ class Calendar extends React.Component {
       })
     }
   }
-
+  //选择日期
   datePick(day) {
     this.setState({day})
   }
-
+  //切换日期选择器是否显示
   datePickerToggle() {
     this.refs.main.style.height =
       this.refs.main.style.height === '460px' ?
         '0px' : '460px'
   }
-
+  //标记日期已经选择
   picked() {
     this.state.picked = true
   }
@@ -134,6 +149,7 @@ class Calendar extends React.Component {
   }
 }
 
+//将calender实例添加到window上以便获取日期选择数据
 window.calendar = render(
   <Calendar/>,
   document.getElementById('calendarContainer')
--- src/components/CalendarMain.js
+++ src/components/CalendarMain.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default class CalendarMain extends React.Component {
 
+  //处理日期选择事件，如果是当月，触发日期选择；如果不是当月，切换月份
   handleDatePick(index, styleName) {
     switch (styleName) {
       case 'thisMonth':
@@ -17,6 +18,9 @@ export default class CalendarMain extends React.Component {
     }
   }
 
+  //处理选择时选中的样式效果
+  //利用闭包保存上一次选择的元素，
+  //在月份切换和重新选择日期时重置上一次选择的元素的样式
   changeColor() {
     let previousEl = null
     return function (event) {
@@ -31,6 +35,7 @@ export default class CalendarMain extends React.Component {
     }
   }
 
+  //绑定颜色改变事件
   componentDidMount() {
     let changeColor = this.changeColor()
     document.getElementById('calendarContainer')
@@ -39,6 +44,7 @@ export default class CalendarMain extends React.Component {
   }
 
   render() {
+    //确定当前月数据中每一天所属的月份，以此赋予不同className
     let month = this.props.viewData[this.props.month],
       rowsInMonth = [],
       i = 0,
@@ -51,6 +57,8 @@ export default class CalendarMain extends React.Component {
         arr.fill('nextMonth', j)
         return arr
       })()
+
+    //把每一个月的显示数据以7天为一组等分
     month.forEach((day, index)=> {
       if (index % 7 === 0) {
         rowsInMonth.push(month.slice(index, index + 7))
@@ -97,4 +105,3 @@ export default class CalendarMain extends React.Component {
     )
   }
 }
-
